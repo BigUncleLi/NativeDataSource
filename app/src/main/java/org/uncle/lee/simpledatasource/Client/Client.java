34528ebb33e08468c7755a46ba40da4292dd39bb
BigUncleLi <BigUncleLi@163.com>
2016-07-06 23:36:47 +0800

    chore: add test code in dataController

--- app/src/main/java/org/uncle/lee/simpledatasource/Client/Client.java
+++ app/src/main/java/org/uncle/lee/simpledatasource/Client/Client.java
@@ -1,11 +1,12 @@
 package org.uncle.lee.simpledatasource.Client;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import org.uncle.lee.simpledatasource.controller.DataController;
 import org.uncle.lee.simpledatasource.dao.Dao;
 import org.uncle.lee.simpledatasource.entity.App;
 import org.uncle.lee.simpledatasource.entity.Contact;
+import org.uncle.lee.simpledatasource.listener.AppListener;
 import org.uncle.lee.simpledatasource.listener.ContactListener;
 
 /**
@@ -14,22 +15,67 @@ import org.uncle.lee.simpledatasource.listener.ContactListener;
 public class Client {
   public static void main(String[] args) {
     DataController instance = DataController.getInstance();
+    Dao<App, String> appDao = instance.appDao();
+    Dao<Contact, String> contactDao = instance.contactDao();
 
-    testContact(instance);
-    testApp(instance);
+    testAppWrite(appDao);
+    testContactWrite(contactDao);
+
+    testAppQuery(appDao);
+    testContactQuery(contactDao);
   }
 
-  private static void testApp(DataController instance) {
-    Dao<Contact, String> contactDao = instance.contactDao();
+  private static void testContactWrite(Dao<Contact, String> contactDao) {
+    List<Contact> contactList = getContactList();
+    contactDao.setListener(new ContactListener() {
+      @Override public void onActionDone(Dao.DaoActionType type, List<Contact> contacts) {
+
+      }
+    });
+    contactDao.insert(contactList);
+  }
+
+  private static List<Contact> getContactList() {
+    List<Contact> contactList = new ArrayList<Contact>();
+    contactList.add(new Contact(Long.parseLong("1"), "张三", "zhangsan", "10086"));
+    contactList.add(new Contact(Long.parseLong("2"), "张三", "zhangsan", "10000"));
+    contactList.add(new Contact(Long.parseLong("3"), "章三", "zhangsan", "123123"));
+    return contactList;
+  }
+
+  private static void testAppWrite(Dao<App, String> appDao) {
+    List<App> appList = getAppList();
+    appDao.setListener(new AppListener() {
+      @Override public void onActionDone(Dao.DaoActionType type, List<App> apps) {
+
+      }
+    });
+    appDao.insert(appList);
+  }
+
+  private static List<App> getAppList() {
+    List<App> appList = new ArrayList<App>();
+    appList.add(new App("packageNameA", "laberA", "pyLaberA", "classNameA"));
+    appList.add(new App("packageNameB", "laberB", "pyLaberB", "classNameB"));
+    appList.add(new App("packageNameC", "laberC", "pyLaberC", "classNameC"));
+    return appList;
+  }
+
+  private static void testAppQuery(Dao<App, String> appDao) {
+    appDao.setListener(new AppListener() {
+      @Override public void onActionDone(Dao.DaoActionType type, List<App> apps) {
+
+      }
+    });
+    appDao.queryAll();
+  }
+
+  private static void testContactQuery(Dao<Contact, String> contactDao) {
     contactDao.setListener(new ContactListener() {
       @Override public void onActionDone(Dao.DaoActionType type, List<Contact> contacts) {
 
       }
     });
     contactDao.queryAll();
-  }
-
-  private static void testContact(DataController instance) {
-    Dao<App, String> appDao = instance.appDao();
   }
 }
