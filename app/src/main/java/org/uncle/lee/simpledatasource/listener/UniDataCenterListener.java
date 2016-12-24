5efa458b0f37f6281611933edcb038b12acc19bf
BigUncleLi <BigUncleLi@163.com>
2016-12-24 23:12:26 +0800

    feat : change UniDataCenterListener interface
    1. delete isSuccess boolean val
    2. add error enum action type

--- app/src/main/java/org/uncle/lee/simpledatasource/listener/UniDataCenterListener.java
+++ app/src/main/java/org/uncle/lee/simpledatasource/listener/UniDataCenterListener.java
@@ -6,12 +6,16 @@ import java.util.List;
  * Created by Austin on 2016/7/7.
  */
 public interface UniDataCenterListener {
-  public abstract void onAction(ActionType actionType, boolean isSuccess, List<?> dataList);
+  public abstract void onAction(ActionType actionType, List<?> dataList);
 
   public enum ActionType{
     INIT_DONE,
+    INIT_ERROR,
     INSERT_DONE,
+    INSERT_ERROR,
     QUERY_ALL_DONE,
-    CLEAN_DONE;
+    QUERY_ALL_ERROR,
+    CLEAN_DONE,
+    CLEAN_ERROR;
   }
 }
