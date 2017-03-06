1f4226724bb6260196328e5c01bf39cc89ae4117
BigUncleLi <BigUncleLi@163.com>
2017-03-06 19:15:03 +0800

    chore : add some notes

--- app/src/main/java/org/uncle/lee/nativedatasource/data/center/DataCenter.java
+++ app/src/main/java/org/uncle/lee/nativedatasource/data/center/DataCenter.java
@@ -11,16 +11,23 @@ import org.uncle.lee.nativedatasource.listener.DataListener;
  * all functions provided for users
  */
 interface DataCenter {
+  // Init
   void setListener(DataListener<?> listener);
   void init(Context mContext);
 
+  // Contact operation
   void queryContactList();
   void insertContactList(List<Contact> contactList);
   void cleanContactList();
 
+  // App operation
   void queryAppList();
   App queryAppByName(String name);
   App queryAppByPackageName(String packageName);
   void insertAppList(List<App> appList);
   void cleanAppList();
+
+  // Music file operation
+
+  // Video file operation
 }
